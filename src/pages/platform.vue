06ee8b3e7fb884509e3f0f5c1b0eb55082180d4f
源尼玛 <jiangtai20@mails.ucas.ac.cn>
2022-06-09 21:40:03 +0800

    init

---
+++ src/pages/platform.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <canvas ref="canvas" width="500" height="500" style="width:100%"></canvas>
+  </div>
+</template>
+<script setup lang="ts">
+import { usePlatform } from '@/store';
+import { computed, onMounted, ref, watch } from 'vue';
+import { Platform } from '@/utils/platform'
+const props = defineProps({
+  id: String
+})
+const store = usePlatform();
+const canvas = ref<HTMLCanvasElement>();
+let platform:any;
+
+watch(() => props.id, (val, old) => {
+  console.log(val, old);
+
+}, {immediate:true});
+
+onMounted(() => {
+  if(canvas.value) {
+    platform = new Platform(canvas.value);
+    platform.start();
+  }
+})
+</script>
